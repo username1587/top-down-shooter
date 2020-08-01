37499ae2e954ddbc46c806664cd7080df0a139c6
username1587 <50865200+paulandrei12345@users.noreply.github.com>
2020-08-01 17:58:45 +0300

    camera follow player
    basic script : camera follows the player
    TODO: make a smoother version of this

---
+++ top down shooter/Assets/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] Vector3 cameraOffset = new Vector3(0, 0, -10);
+
+    // TODO: make camera follow the player smooth
+    void Update()
+    {
+        transform.position = player.position + cameraOffset;
+    }
+}
