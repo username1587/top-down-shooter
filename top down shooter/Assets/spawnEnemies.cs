c1b86441a1ce4cf5e10b74777bcb29e80d5c48c3
username1587 <50865200+paulandrei12345@users.noreply.github.com>
2020-08-05 14:51:17 +0300

    added enemy spawner

---
+++ top down shooter/Assets/spawnEnemies.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnEnemies : MonoBehaviour
+{
+    public int wave = 1;
+    public GameObject enemy;
+    public Transform parrentInHierarchy;
+    public Transform positionToInstantiate;
+    public float timer=3f;
+
+    private float time;
+
+    private void Start()
+    {
+        time = 0;
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= timer)
+        {
+            time = 0;
+            for (int i = 0; i < wave; i++)
+            {
+                var _enemy = Instantiate(enemy, positionToInstantiate.position, enemy.transform.rotation, parrentInHierarchy);
+                setEnemyTarget(_enemy);
+            }
+
+        }
+    }
+
+    void setEnemyTarget(GameObject enemy)
+    {
+        var player = FindObjectOfType<Move>();
+        enemy.GetComponent<Pathfinding.AIDestinationSetter>().target = player.transform;
+    }
+}
